0802e0806b8eb0a6a4bde3862cb5ad7bcec054d7
Steve Zhang <huigang.zhang@gmail.com>
2025-01-01 00:15:42 -0500

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyuiPlugin from 'daisyui';
 
 export default {
   content: [
@@ -14,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyuiPlugin],
 } satisfies Config;
