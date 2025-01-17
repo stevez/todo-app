c284aaba14ecf08bdbb87677fe32e01cbb9d4c24
Steve Zhang <huigang.zhang@gmail.com>
2025-01-16 23:30:17 -0500

    support parallel testing

--- e2e/mock-server-api.ts
+++ e2e/mock-server-api.ts
@@ -1,20 +1,21 @@
-import { expect } from "@playwright/test";
+import { APIRequestContext, expect } from "@playwright/test";
 
 const mockServerUrl = "http://localhost:3001";
 
-export const overrideResponse = async (request, body) => {
+export const overrideResponse = async (request: APIRequestContext, body, headers) => {
   const response = await request.post(`${mockServerUrl}/override/register`, {
     data: body,
+    headers,
   });
   expect(response.ok()).toBeTruthy();
 };
 
-export const resetOverrideResponse = async (request) => {
-  const response = await request.post(`${mockServerUrl}/override/reset`);
+export const resetOverrideResponse = async (request:APIRequestContext, headers) => {
+  const response = await request.post(`${mockServerUrl}/override/reset`, {headers});
   expect(response.ok()).toBeTruthy();
 };
 
-export const resetDB = async (request) => {
-  const response = await request.post(`${mockServerUrl}/resetdb`);
+export const resetDB = async (request, headers) => {
+  const response = await request.post(`${mockServerUrl}/resetdb`, {headers});
   expect(response.ok()).toBeTruthy();
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import TodoList from "./components/TodoList";
 
 export default async function Home() {
   const tasks = await getAllTodos();
-  console.log(tasks);
   return (
     <main className="max-w-4xl mx-auto mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
